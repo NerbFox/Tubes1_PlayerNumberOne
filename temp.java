cb3b45f52958cb07eb8f29e34d1ba5821724e301
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-02-15 20:40:21 +0700

    Update Algorithm Valid object (greedy)

--- temp.java
+++ temp.java
@@ -32,12 +32,10 @@ public class BotService {
         this.playerAction = playerAction;
     }
 
-    private Boolean isNextTargetValid(GameObject object, List<GameObject> gasCloudList,
-            List<GameObject> asteroidFieldList, List<GameObject> playerList) {
-        var distError = 5;
+    private Boolean isFoodValid(GameObject food, List<GameObject> gasCloudList, List<GameObject> asteroidFieldList) {
         if (!gasCloudList.isEmpty()) {
             for (GameObject gasCloud : gasCloudList) {
-                if (getDistanceBetween(object, gasCloud) + distError <= gasCloud.size) {
+                if (getDistanceBetween(food, gasCloud) <= gasCloud.size) {
                     System.out.println("Food not valid - Gas Clouds Ahead");
                     return false;
                 }
@@ -46,29 +44,11 @@ public class BotService {
 
         if (!asteroidFieldList.isEmpty()) {
             for (GameObject asteroidField : asteroidFieldList) {
-                if (getDistanceBetween(object, asteroidField) + distError <= asteroidField.size) {
+                if (getDistanceBetween(food, asteroidField) <= asteroidField.size) {
                     System.out.println("Food not valid - Asteroid Fields Ahead");
                     return false;
                 }
             }
-        }
-        // heading if ini food baru lakuin ini
-        var distNear = 10;
-        var degNear = 5;
-        if (!playerList.isEmpty()) {
-            var dist = getDistanceBetween(object, playerList.get(1)) - bot.size - playerList.get(0).getSize();
-            if ((dist <= distNear) && (getHeadingBetween(object) <= (getHeadingBetween(playerList.get(1)) + distNear))
-                    && (getHeadingBetween(object) >= getHeadingBetween(playerList.get(1)) - distNear)) {
-                System.out.println("Food not valid - Object heading not good");
-                return false;
-            }
-            // for(GameObject player : playerList ){
-
-        }
-        // inBorder
-        if (inBorderValid(object)) {
-            System.out.println("Food not valid - Food mau keluar");
-            return false;
         }
 
         System.out.println("Food valid");
@@ -76,24 +56,10 @@ public class BotService {
 
     }
 
-    private boolean inBorderValid(GameObject object) {
-        boolean inBorder = false;
-        double distToBorderMin = 15.0;
-        var rad = gameState.getWorld().getRadius();
-
-        if (!gameState.getGameObjects().isEmpty()) {
-            if ((rad - (getDistancePosition(object, 0, 0) + object.getSize())) <= distToBorderMin) {
-                inBorder = true;
-                System.out.println("inBorder");
-            } else {
-                inBorder = false;
-                System.out.println("outBorder");
-            }
-        }
-        return inBorder;
-    }
-
     public void computeNextPlayerAction(PlayerAction playerAction) {
+        playerAction.action = PlayerActions.FORWARD;
+        playerAction.heading = new Random().nextInt(360);
+
         var torpedoList = gameState.getGameObjects().stream()
                 .filter(item -> item.getGameObjectType() == ObjectTypes.TORPEDOSALVO)
                 .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item))).collect(Collectors.toList());
@@ -101,14 +67,16 @@ public class BotService {
                 .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item))).collect(Collectors.toList());
         var foodList = gameState.getGameObjects().stream()
                 .filter(item -> (item.getGameObjectType() == ObjectTypes.FOOD
-                        || (item.getGameObjectType() == ObjectTypes.SUPERFOOD)
-                        || (item.getGameObjectType() == ObjectTypes.SUPERNOVAPICKUP)))
+                        || (item.getGameObjectType() == ObjectTypes.SUPERFOOD)))
                 .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item))).collect(Collectors.toList());
         var wormholeList = gameState.getGameObjects().stream()
                 .filter(item -> item.getGameObjectType() == ObjectTypes.WORMHOLE)
                 .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item))).collect(Collectors.toList());
         var gasCloudList = gameState.getGameObjects().stream()
                 .filter(item -> item.getGameObjectType() == ObjectTypes.GASCLOUD)
+                .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item))).collect(Collectors.toList());
+        var asteroidFieldList = gameState.getGameObjects().stream()
+                .filter(item -> item.getGameObjectType() == ObjectTypes.ASTEROIDFIELD)
                 .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item))).collect(Collectors.toList());
         var superfoodList = gameState.getGameObjects().stream()
                 .filter(item -> item.getGameObjectType() == ObjectTypes.SUPERFOOD)
@@ -119,60 +87,53 @@ public class BotService {
         var supernovaBombList = gameState.getGameObjects().stream()
                 .filter(item -> item.getGameObjectType() == ObjectTypes.SUPERNOVABOMB)
                 .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item))).collect(Collectors.toList());
-        var asteroidFieldList = gameState.getGameObjects().stream()
-                .filter(item -> item.getGameObjectType() == ObjectTypes.ASTEROIDFIELD)
-                .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item))).collect(Collectors.toList());
-                
 
-        // algoritma yang diinginkan adalah: greedy, kalo ada food yang searah sama bot
-        // lawan, maka bot akan menghindar !! lalu jika tidak ada food yang searah, maka
-        // bot akan mengambil food yang terdekat
-        // size itu radius apa diameter?
-        // di valid food tambahin inborder
-        // n heding klo ada food yang searah sama bot lawan
-
-        int botSize = 30;
-        boolean inBorder = false;
-        playerAction.heading = new Random().nextInt(360);
-        // if (bot.size < botSize && !superfoodList.isEmpty() && !inBorder){
+        // Harus ditambah kasus food abis
+        // System.out.println("food");
+        // System.out.println(foodList);
+        // System.out.println("gascloud");
+        // System.out.println(gasCloudList);
+        // System.out.println("asteroid");
+        // System.out.println(asteroidFieldList);
         if (!gameState.getGameObjects().isEmpty()) {
-            Integer foodIndex = 0;
-            var foodTarget = foodList.get(foodIndex);
-            while (!isNextTargetValid(foodTarget, gasCloudList, asteroidFieldList, playerList)) {
-                System.out.println("test");
-                foodIndex++;
-                foodTarget = foodList.get(foodIndex);
-            }
-            // if(supernovaList.isEmpty())
-            if (bot.getSupernovaAvailable() > 0) {
-                playerAction.heading = getHeadingBetween(playerList.get(1));
-                playerAction.action = PlayerActions.FIRESUPERNOVA;
-            } else {
-                if (bot.size < botSize) {
-                    playerAction.action = PlayerActions.FORWARD;
-                    if (!gameState.getGameObjects().isEmpty()) { // if there are any game objects
-                        playerAction.heading = getHeadingBetween(foodTarget);
+            if (!foodList.isEmpty()) {
+                Integer foodIndex = 0;
+                var foodTarget = foodList.get(foodIndex);
+                while (!isFoodValid(foodTarget, gasCloudList, asteroidFieldList)) {
+                    System.out.println("test");
+                    foodIndex++;
+                    foodTarget = foodList.get(foodIndex);
+                }
 
-                        // if (!inBorder) {
-                        // playerAction.heading = getHeadingBetween(foodList.get(0));
-                        // } else {
-                        // playerAction.heading = getHeadingBetween(foodList.get(1));
-                        // }
-                    }
-                } else {
-                    if (bot.size > playerList.get(1).size) {
-                        playerAction.heading = getHeadingBetween(playerList.get(1));
+                if (bot.size < 50 || getDistanceBetween(bot, playerList.get(1)) > 30) {
+                    var distance = getDistanceBetween(bot, playerList.get(1));
+                    if (distance - playerList.get(1).size - bot.size <= 25 && bot.size < playerList.get(1).size) {
+                        playerAction.heading = playerList.get(1).getCurrentHeading();
                         playerAction.action = PlayerActions.FORWARD;
+                        System.out.println("Kaboor");
+                        if (bot.torpedoCount >= 3) {
+                            playerAction.heading = getHeadingBetween(playerList.get(1));
+                            playerAction.action = PlayerActions.FIRETORPEDOES;
+                        }
                     } else {
-                        playerAction.heading = getHeadingBetween(playerList.get(1));
-                        playerAction.action = PlayerActions.FIRETORPEDOES;
+                        playerAction.heading = getHeadingBetween(foodTarget);
+                        System.out.println("eat food");
                     }
+                    // if (bot.effects == 1 || bot.effects == 3 || bot.effects == 5 || bot.effects
+                    // == 7) {
+                    // playerAction.action = PlayerActions.FORWARD;
+                    // } else {
+                    // playerAction.action = PlayerActions.STARTAFTERBURNER;
+                    // System.out.println("START NITRO");
+                    // }
+
+                } else {
+                    playerAction.heading = getHeadingBetween(playerList.get(1));
+                    playerAction.action = PlayerActions.FIRETORPEDOES;
+                    System.out.println("ATK");
                 }
             }
-            // else{
-            // playerAction.action = PlayerActions.FORWARD;
-            // playerAction.heading = getHeadingBetween(supernovaList.get(0));
-            // }
+
         }
 
         this.playerAction = playerAction;
@@ -196,12 +157,6 @@ public class BotService {
     private double getDistanceBetween(GameObject object1, GameObject object2) {
         var triangleX = Math.abs(object1.getPosition().x - object2.getPosition().x);
         var triangleY = Math.abs(object1.getPosition().y - object2.getPosition().y);
-        return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
-    }
-
-    private double getDistancePosition(GameObject object1, int x, int y) {
-        var triangleX = Math.abs(object1.getPosition().x - x);
-        var triangleY = Math.abs(object1.getPosition().y - y);
         return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
     }
 
